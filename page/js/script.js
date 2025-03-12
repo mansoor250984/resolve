360ddb823f757962d9ee358ca29bf75a0b4d7e60
mansoor250984 <mansoor250984@gmail.com>
2025-03-12 10:06:07 +0530

    Add files via upload

---
+++ page/js/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function() { 
+						   
+function handleResize() {
+        const $element = $('.nav');
+        if ($(window).width() < 1025) {
+          $element.addClass('mobile');
+        } else {
+          $element.removeClass('mobile');
+        }
+      }
+ // Initial check
+      handleResize();
+
+      // Bind the function to the window resize event
+      $(window).resize(function() {
+        handleResize();
+      });					   
+						   
+						   
+						   
+$(".menu-bar").click(function(){
+    $(".nav div.menu, .overlay").toggle(400);
+});
+$(".menu .fa-bars").click(function(){
+    $(".nav div.menu, .overlay").toggle(400);
+});
+
+$(".popup .closeBtn").click(function(){
+    $(".popup").hide(100);
+});
+
+
+$(".mobile ul li a").click( function ( e ){ 
+   e.preventDefault()
+   $( this ).parent().find( ".sub-menu" ).toggle(500).toggleClass("hidden", 500) } )
+
+
+
+
+
+
+});
